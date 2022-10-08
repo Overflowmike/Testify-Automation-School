0a9e56d759d9a1ca1d96b8db50a831b5857a63bc
Michael Alabi <alabimichael23@gmail.com>
2022-10-08 12:11:18 +0100

    Update Challenge1.js file

---
+++ JavaScript Programming Language/Challenge1.js
@@ -0,0 +1,8 @@
+// Calculate the sum of numbers within an array
+
+let numbers = [60, 65, 73, 85, 45, 90];
+//const sum = numbers.reduce((result,number)=>result+number);
+const sum = (60 + 65 + 73 + 85 + 45 + 90)
+ console.log(sum)
+  
+
