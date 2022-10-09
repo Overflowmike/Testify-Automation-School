d0241f07bd39981749cefafb2c07c9f7982e1ecd
Michael Alabi <alabimichael23@gmail.com>
2022-10-09 23:44:44 +0100

    Add Python Programming Language

---
+++ Python Programming Language/Task6.py
@@ -0,0 +1,15 @@
+# Task 6
+
+number1 = 70
+number2 = 175
+
+if number1 > 50 and number2 < 100:
+    print("Evaluation is True")
+else:
+    print("Evaluation is False")
+
+programmingLanguage = "python"
+automationSchool = "testify"
+
+if programmingLanguage == "python" or automationSchool == "testify":
+    print("Yay!! The string is a Testify Python ")
