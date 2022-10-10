c96c60dd901aff5584547099012bc02279e7fb2d
Michael Alabi <alabimichael23@gmail.com>
2022-10-10 08:02:22 +0100

    Add Task 8- py

---
+++ Python Programming Language/Task8.py
@@ -0,0 +1,24 @@
+# Task 8
+
+number = 11
+for i in range(number):
+    if i == 11:
+        break
+    print("Number:", i)
+
+print("\ncontinue\n")
+
+number = 11
+for i in range(number):
+    if i == 2:
+        continue
+    print("for: Number:", i)
+
+
+print("\nbreak\n")
+
+number = 11
+for i in range(number):
+    if i == 8:
+        break
+    print("for: Number:", i)
