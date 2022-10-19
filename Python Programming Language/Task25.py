9c483dc9001d8b284218c32c7d47233fb283fc63
Michael Alabi <alabimichael23@gmail.com>
2022-10-19 23:26:58 +0100

    Add Python Programming Language

--- Python Programming Language/Task25.py
+++ Python Programming Language/Task25.py
@@ -1,8 +1,6 @@
 # Task 25
 
-
 import print_name as print_name
-
 
 class Utilities:
 
@@ -18,7 +16,12 @@ class Utilities:
     def print_name3(self):
         return "Gas"
 
+    @staticmethod
+    def print_name4(self):
+        return "Heater"
+
 printName = staticmethod(print_name)
 print("Utility1 =", Utilities.print_name("Electricity"))
 print("Utility2 =", Utilities.print_name2("Water Board"))
 print("Utility3 =", Utilities.print_name3("Gas"))
+print("Utility4 =", Utilities.print_name4("Heater"))
