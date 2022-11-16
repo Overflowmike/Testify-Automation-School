f56c3741a788430d9c90c9e8286423d5f29f091f
Michael Alabi <alabimichael23@gmail.com>
2022-11-17 00:23:41 +0100

    Module 4 Python Automation Task 5 POM

---
+++ Python Coding Challenge/PythonCoding_Challenge1.py
@@ -0,0 +1,9 @@
+
+# Create  a  two-string  variable,  concatenate  the  two  variablesand print out the result
+
+group = "Laptop"
+attr = "Hp"
+
+ComputerName = attr  +  group
+
+print(ComputerName)
