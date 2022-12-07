9e8a429a9cf0448ecdac9a942bf08ad56161e687
Michael Alabi <alabimichael23@gmail.com>
2022-12-07 21:46:09 +0100

    Mobile Calculator Automation

---
+++ appium_mobile/calculator_automation.py
@@ -0,0 +1,25 @@
+import time
+
+from appium import webdriver
+from appium.webdriver.common.mobileby import MobileBy
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+def main():
+    desired_caps = {
+        "deviceName": "Android Emulator",
+        "platformName": "Android",
+        "platformVersion": "12",
+        "app": "C:/Users/Michael Alabi/Test Automation Training X/Calculator_8.3 (477856174)_Apkpure.apk",
+        "appPackage": "com.google.android.calculator",
+         "noSign": True
+    }
+    driver = webdriver.Remote(command_executor="http://127.0.0.1:4723/wd/hub", desired_capabilities=desired_caps)
+    # deviceOnlyElement = WebDriverWait(driver, 5).until(EC.element_to_be_clickable((MobileBy.XPATH, "//android.widget.Button[@text='DEVICE FILES ONLY']")))
+    # deviceOnlyElement.click()
+    time.sleep(5)
+    driver.quit()
+
+if __name__ == '__main__':
+    main()
