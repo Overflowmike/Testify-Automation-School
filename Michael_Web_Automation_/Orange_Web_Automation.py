0a3a923d02aa37f5519e6a137dc5ea2aeed235c7
Michael Alabi <alabimichael23@gmail.com>
2022-12-12 23:33:27 +0100

    Practice Web Automation

---
+++ Michael_Web_Automation_/Orange_Web_Automation.py
@@ -0,0 +1,36 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+def send_keys_to_element(element, *keys):
+    element.send_keys(keys)
+
+def main():
+    #Launch browser
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    driver.get("https://opensource-demo.orangehrmlive.com/")
+    time.sleep(4)
+    # Enter Username and Password
+    send_keys_to_element(driver.find_element(By.NAME, "username"), "Admin")
+    time.sleep(2)
+    send_keys_to_element(driver.find_element(By.XPATH, '//*[@id="app"]/div[1]/div/div[1]/div/div[2]/div[2]/form/div[2]/div/div[2]/input'), "admin123")
+    time.sleep(3)
+    # Log in
+    log_in_button = driver.find_element(By.XPATH, '/html/body/div/div[1]/div/div[1]/div/div[2]/div[2]/form/div[3]/button')
+    log_in_button.find_element(By.XPATH, '/html/body/div/div[1]/div/div[1]/div/div[2]/div[2]/form/div[3]/button').click()
+    time.sleep(10)
+
+   # Log out
+    profile_dropdown = driver.find_element(By.XPATH, '//*[@id="app"]/div[1]/div[1]/header/div[1]/div[2]/ul/li/span/p')
+    profile_dropdown.find_element(By.XPATH, '//*[@id="app"]/div[1]/div[1]/header/div[1]/div[2]/ul/li/span/p').click()
+    time.sleep(5)
+    log_out_button = driver.find_element(By.XPATH, '//*[@id="app"]/div[1]/div[1]/header/div[1]/div[2]/ul/li/ul/li[4]/a')
+    log_out_button.find_element(By.XPATH, '//*[@id="app"]/div[1]/div[1]/header/div[1]/div[2]/ul/li/ul/li[4]/a').click()
+    time.sleep(5)
+    driver.close()
+
+
+if __name__ == '__main__':
+    main()
